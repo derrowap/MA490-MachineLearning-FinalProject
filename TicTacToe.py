c8237bed896fd29c1f192c248d7aff3f80b1b342
Austin Derrow-Pinion <derrowap@rose-hulman.edu>
2016-04-27 10:38:10 -0400

    fixed whoGoesFirst() and int print statements

--- TicTacToe.py
+++ TicTacToe.py
@@ -7,7 +7,7 @@ def main():
 	"""
 	Game = TicTacToe()
 	while True:
-		print("Player%i, take your move." % Game.turn)
+		print("Player%d, take your move." % Game.turn)
 		row = int(raw_input("Enter row of move..."))
 		col = int(raw_input("Enter col of move..."))
 		Game.move(Game.turn, row, col)
@@ -61,10 +61,10 @@ class TicTacToe():
 		If that position has already been taken, will alert player and it will still be their turn.
 		"""
 		if player != self.turn: # not this player's turn
-			print("Oops! It is not your turn, Player%i" % player)
+			print("Oops! It is not your turn, Player%d" % player)
 			return
 		if self.state[row][col]: # already a move in this position
-			print("Oops! It seems that someone has already moved here, Player%i" % player)
+			print("Oops! It seems that someone has already moved here, Player%d" % player)
 			return
 		else: # empty spot
 			self.state[row][col] = player
@@ -83,21 +83,23 @@ class TicTacToe():
 			(board[0][0] == player and board[1][0] == player and board[2][0] == player) or
 			(board[0][1] == player and board[1][1] == player and board[2][1] == player) or
 			(board[0][2] == player and board[1][2] == player and board[2][2] == player)):
-			print("Yay! Player%i is the winner!" % player)
+			print("----------------------------")
+			print("Yay! Player%d is the winner!" % player)
+			print("----------------------------")
 			self.win = player
 
 	def whoGoesFirst(self):
 		"""
 		Randomly chooses either 1 or 2, representing what player goes first in this game.
 		"""
-		self.turn = random.randint(1, 2)
+		return random.randint(1, 2)
 
 	def restartGame(self):
 		"""
 		Restarts this game's values to a new game.
 		"""
 		self.state = [[0 for x in range(3)] for y in range(3)]
-		self.whoGoesFirst()
+		self.turn = self.whoGoesFirst()
 		self.win = 0
 
 	def printBoard(self):
@@ -118,4 +120,4 @@ class TicTacToe():
 		print('   |   |')
 
 if __name__ == "__main__":
-	main()				
+	main()		
