9e76478b46725ab3213a994d520bfaf1ffd72a98
Austin Derrow-Pinion <derrowap@rose-hulman.edu>
2016-05-05 18:12:05 -0400

    Fixed to proper formatting

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,11 +1,9 @@
-# import numpy as np
 import random
 
 def main():
-	"""
-	Begins a game of tic-tac-toe.
-	"""
+	"""Begins a game of tic-tac-toe."""
 	Game = TicTacToe()
+	print("Welcome to Tic-Tac-Toe")
 	while True:
 		print("Player%d, take your move." % Game.turn)
 		row = int(input("Enter row of move... "))
@@ -20,45 +18,38 @@ def main():
 				print("Closing Tic-Tac-Toe Game...")
 				return
 
-"""
-A tic-tac-toe game. Two players take turns specifying the row and column to make
-their next move. When a player wins, they can either exit or restart.
-"""
 class TicTacToe():
+	"""A tic-tac-toe game. 
 
+	Two players take turns specifying the row and column to make
+	their next move. When a player wins, they can either exit or restart.
+
+	Attributes
+	----------
+	state: 3x3 integer array
+		Current state of the game where 0 is empty, 1 for X, and 2 for O.
+	turn: integer
+		Represents whos turn it currently is. 1 for X and 2 for O.
+	win: integer
+		0 if there is no current winner. 1 if X wins, 2 if O wins.
+	"""
 	def __init__(self):
-		# State is a 3x3 matrix representation of the current game
-		# A game that looks like:
-		# X |   | O
-		# ---------
-		# O |   | X
-		# ---------
-		#   | X | O
-		# The matrix will be:
-		# [ [1, 0, 2], [2, 0, 1], [0, 1, 2] ]
-		# Such that empty space is 0, 'X' is 1, and 'O' is 2.
 		self.state = [[0 for x in range(3)] for y in range(3)]
-		# If turn is 1, it is Player1's turn.
-		# If turn is 2, it is Player2's turn.
 		self.turn = self.whoGoesFirst()
-		# If win is 0, nobody wins.
-		# If win is 1, Player1 wins.
-		# If win is 2, Player2 wins.
 		self.win = 0
 
 	def changeTurn(self):
-		"""
-		Flips who turn it is in this game.
-		"""
+		"""Flips who turn it is in this game."""
 		if self.turn == 1:
 			self.turn = 2
 		else:
 			self.turn = 1
 
 	def move(self, player, row, col):
-		"""
-		Moves the player at the given row and col in the board.
-		If that position has already been taken, will alert player and it will still be their turn.
+		"""Moves the player at the given row and col in the board.
+
+		If that position has already been taken, will alert player and 
+		it will still be their turn.
 		"""
 		if player != self.turn: # not this player's turn
 			print("Oops! It is not your turn, Player%d" % player)
@@ -72,9 +63,7 @@ class TicTacToe():
 			self.changeTurn()
 
 	def checkForWin(self, board, player):
-		"""
-		Given a player, checks all possibilities to see if they win.
-		"""
+		"""Checks all possibilities for player to win."""
 		if ((board[0][0] == player and board[0][1] == player and board[0][2] == player) or
 			(board[1][0] == player and board[1][1] == player and board[1][2] == player) or
 			(board[2][0] == player and board[2][1] == player and board[2][2] == player) or
@@ -89,23 +78,20 @@ class TicTacToe():
 			self.win = player
 
 	def whoGoesFirst(self):
-		"""
-		Randomly chooses either 1 or 2, representing what player goes first in this game.
+		"""Assigns what player goes first in this game.
+
+		Randomly chooses either 1 or 2, representing which player.
 		"""
 		return random.randint(1, 2)
 
 	def restartGame(self):
-		"""
-		Restarts this game's values to a new game.
-		"""
+		"""Restarts this game's values to a new game."""
 		self.state = [[0 for x in range(3)] for y in range(3)]
 		self.turn = self.whoGoesFirst()
 		self.win = 0
 
 	def printBoard(self):
-		"""
-		Prints the current state of the tic-tac-toe board.
-		"""
+		"""Prints the current state of the tic-tac-toe board."""
 		key = [' ', 'X', 'O']
 		print('   |   |')
 		print(' ' + key[self.state[0][0]] + ' | ' + key[self.state[0][1]] + ' | ' + key[self.state[0][2]])
--- trainingFunctions.py
+++ trainingFunctions.py
@@ -7,7 +7,8 @@ def main():
 	print("oddParity(2) ->", oddParity(2))
 	print("oddParity(3) ->", oddParity(3))
 	print("oddParity(10) ->", oddParity(10))
-	print("oddParity(1243) + evenParity(1243) == 1 ->", oddParity(1243) + evenParity(1243) == 1)
+	print("oddParity(1243) + evenParity(1243) == 1 ->", 
+		   oddParity(1243) + evenParity(1243) == 1)
 	print("adder(1) ->", adder(1))
 	print("adder(5) ->", adder(5))
 	print("multiply(3, 7) ->", multiply(3, 7))
@@ -16,7 +17,7 @@ def main():
 	print("isPalindrome('yootnooy') ->", isPalindrome('yootnooy'))
 	print("makePalindrome('This is the final countdown!') ->",
 		makePalindrome('This is the final countdown!'))
-	print("isPalindrome(makePalindrome('This is the final countdown!')) ->",
+	print("isPalindrome(makePalindrome('This is the final countdown!')) ->", 
 		isPalindrome(makePalindrome('This is the final countdown!')))
 	print("sine(math.pi) ->", sine(math.pi))
 	print("sine(math.pi / 2) ->", sine(math.pi / 2))
@@ -26,66 +27,60 @@ def main():
 	print("fib(5) ->", fib(5))
 	print("fib(6) ->", fib(6))
 	fn, count, approx = testFibAccuracy()
-	print("Actual", count, "number in fib sequence =", fn, "Approximation =", approx)
-
-
+	print("Actual", count, "number in fib sequence =", fn, 
+		"Approximation =", approx)
 
 def evenParity(n):
-	"""Calculates the number of 1 bits in the
-	binary representation of the given integer, n.
-	If the number is even, outputs a 0.
-	If the number is odd,  outputs a 1.
+	"""Determines if n is an even parity.
+
+	Calculates the number of 1 bits in the binary representation of the given 
+	integer, n. If the number is even, outputs a 0. If the number is odd, 
+	outputs a 1.
 	"""
 	binary =  "{0:b}".format(n)
 	return binary.count('1') % 2
 
 def oddParity(n):
-	"""Calculates the number of 1 bits in the
-	binary representation of the given integer, n.
-	If the number is odd,  outputs a 0.
-	If the number is even, outputs a 1.
+	"""Determines if n is an odd parity.
+
+	Calculates the number of 1 bits in the binary representation of the given 
+	integer, n. If the number is odd,  outputs a 0. If the number is even, 
+	outputs a 1.
 	"""
 	binary = "{0:b}".format(n)
 	return (binary.count('1') + 1) % 2
 
 def adder(n):
-	"""Adds 42 to the given integer, n.
-	"""
-	# 42 is the answer to the universe
+	"""Returns n added by 42."""
 	return n + 42
 
 def multiply(n, m):
-	"""Multiplies the two given integers, n and m.
-	"""
+	"""Returns n multiplied by m."""
 	return n * m
 
 def multiplyAndAdd(n, m):
-	"""Adds 5 the given integer n,
-	adds 7 to the given integer m,
-	and returns the result of these two numbers multiplied.
-	"""
+	"""Adds 5 to n, adds 7 to m, and returns the product of the two."""
 	return (n + 5) * (m + 7)
 
 def makePalindrome(s):
-	"""Returns a string with the given string s
-	reversed and appended to the end of s.
-	This guarantees the return statement is a palindrome.
+	"""Returns a palindrome of s.
+
+	Reverses and appends s to the end of s. This guarantees the return 
+	statement is a palindrome.
 	"""
 	return s + s[::-1]
 
 def isPalindrome(s):
-	"""Returns true if the given string, s, is a valid
-	palindrome. Else, will return false.
-	"""
+	"""Returns true if s is a valid palindrome, else returns false."""
 	return all(s[i] == s[-i - 1] for i in range(len(s) >> 1))
 
 def sine(x):
-	"""Returns the sin of x radians.
-	"""
+	"""Returns the sin of x radians."""
 	return math.sin(x)
 
 def fib(n):
 	"""Returns the nth fibonacci number.
+
 	Since this uses Binet's formula, it is only exactly accurate from 1 to 70.
 	Past n values of 70, it is only an approximation.
 	"""
@@ -93,6 +88,7 @@ def fib(n):
 
 def testFibAccuracy():
 	"""Calculates when the function fib(n) becomes inaccurate.
+
 	Exactly up to 70.
 	71 - 72 are off by 1.
 	73 and on are off by more than 1.
