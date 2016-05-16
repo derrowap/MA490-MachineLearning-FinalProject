a71f90051ff473567c0099d560021acca499de8a
Aaron Bartee <barteeaj@rose-hulman.edu>
2016-05-16 14:15:18 -0400

    I tried

--- nnSine3.py
+++ nnSine3.py
@@ -8,42 +8,32 @@ from trainingFunctions import sine
 from sklearn.metrics import mean_squared_error
 from tensorflow.contrib import skflow
 
-class myThread(threading.Thread):
-	def __init__(self, size):
-		threading.Thread.__init__(self)
-		self.size = size
-		self.x = np.zeros(size)
-		self.y = np.zeros(size)
-	def run(self):
-		for i in range(self.size):
-			b = float(np.random.randint(0, 1000))
-			a = b * (np.pi/180)
-			self.x[i] = a
-			self.y[i] = sine(a)
-#Create a thread and start it
-thread1 = myThread(1000000)
-thread1.start()
 #Inputs and targets
-input_ = thread1.x
-target = thread1.y
+input_ = np.zeros(721)
+target = np.zeros(721)
+for i in range(721):
+	b = i
+	a = b * (np.pi/180)
+	input_[i] = a
+	target[i] = sine(a)
 #Split into training and testing
-x_train, x_test, y_train, y_test = train_test_split(input_, target,
-	test_size=0.2, random_state=0)
+#x_train, x_test, y_train, y_test = train_test_split(input_, target,
+#	test_size=0.2, random_state=0)
 #Neural Network
-NN = skflow.TensorFlowDNNRegressor(hidden_units=[2], steps=10000, learning_rate=0.1)
+NN = TensorFlowDNNRegressor(hidden_units=[5,5,5], steps=10000, learning_rate=0.1)
 #Fit the training data
-NN.fit(x_train, y_train)
+NN.fit(input_, target)
 #Calculates training error
-pred = NN.predict(x_train)
-pred = np.reshape(pred, -1)
-mse_train = mean_squared_error(y_train, pred)
+pred = NN.predict(input_)
+#pred = np.reshape(pred, -1)
+mse_train = mean_squared_error(target, pred)
 #Calculates testing error
-pred = NN.predict(x_test)
-pred = np.reshape(pred, -1)
-mse_test = mean_squared_error(y_test, pred)
+#pred = NN.predict(x_test)
+#pred = np.reshape(pred, -1)
+#mse_test = mean_squared_error(y_test, pred)
 
 print("Training mse is: %0.3f" % mse_train)
-print("Testing mse is: %0.3f" % mse_test)
+#print("Testing mse is: %0.3f" % mse_test)
 
 # Enters loop to predict inputs from user.
 print("\nEnter exit to leave loop.")
